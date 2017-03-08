1060132f0f43d5489ba0b4f5630d5cf0617f7097
Kristin Caron <krissycaron@gmail.com>
2017-03-07 21:28:39 -0600

    started js and console-logged the data from the JSON

---
+++ pets.js
@@ -0,0 +1,57 @@
+console.log("first line in JS file", Date.now());
+
+var petFoodContainer = document.getElementById("petFoodDiv");
+
+function makeDogDom (xhrData) {
+var dogBrandString = "";
+var currentBrand; 
+var currentDogBrandType = "";
+	for(var i=0; i<xhrData.dog_brands.length; i++) {	
+		currentBrand = xhrData.dog_brands[i];
+		console.log(currentBrand);
+		// eneter code into the thml and then copy it in here for how it should work ... 
+		 
+		for(var j=0; j<xhrData.dog_brands[i].types.length; j++) { 
+			currentDogBrandType = xhrData.dog_brands[i].types[j]; // nested needs new index name or letter and need to need first for loop when doing second for loop
+			console.log(currentDogBrandType);	
+		}
+	} 
+}
+
+// function makeDom(xhrData) {
+// 	var dinosaursString = "";
+// 	var currentDinosaur;
+// 	for(var i=0; i<xhrData.dinosaurs.length; i++){
+// 		currentDinosaur = xhrData.dinosaurs[i];
+	
+// 	  dinosaursString += `<div class="col-sm-6 col-md-4">`; //copied and pasted from bootstrap 
+// 	  dinosaursString += `<div class="thumbnail">`; // must use your "`" tick marks
+// 	  dinosaursString += `<img src="${currentDinosaur.url}" alt="dino">`;
+// 	  dinosaursString += `<div class="caption">`;
+// 	  dinosaursString += `<h3>${currentDinosaur.name}</h3>`;
+// 	  dinosaursString += `<p>Is a ${currentDinosaur.type}</p>`;
+// 	  dinosaursString += `<p>Likes to eat: ${currentDinosaur.food}</p>`;
+// 	  dinosaursString += `</div></div></div>`
+// 	}
+// 	dinoContainer.innerHTML = dinosaursString;
+// }
+function executeThisCodeAfterFileLoaded(){
+	console.log("Data Returned: ", Date.now());
+	var data =JSON.parse(this.responseText);
+	makeDogDom(data);
+	console.log(data);
+}
+
+function executeThisCodeAfterFileFails(){
+	console.log("it DID NOT worked!")
+}
+
+var dogRequest = new XMLHttpRequest();
+dogRequest.addEventListener("load", executeThisCodeAfterFileLoaded); // happens when the file has completed reading the JSON file
+dogRequest.addEventListener("error", executeThisCodeAfterFileFails);
+dogRequest.open("GET", "dogs.json"); 
+dogRequest.send();
+
+
+
+console.log("last line in JS file", Date.now());
